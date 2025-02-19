0f07b31744b44a2b8cdf93e155181377790dd606
Imran Syam <md.imransyam1956@gmail.com>
2025-02-19 16:55:30 +0600

    debug

--- app/components/room/room-availablity-calender.tsx
+++ app/components/room/room-availablity-calender.tsx
@@ -5,7 +5,7 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 // import { startOfWeek } from "date-fns";
 
-type Booking = {
+type Bookingpeisma = {
   startTime: string;
   endTime: string;
 };
